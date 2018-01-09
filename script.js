aa225304950af0efde4c8efcfcb939209824ba9f
Lydia Wozniak <lydiawozniak@Lydias-MacBook-Pro.local>
2018-01-09 13:02:28 +0000

    use of getelementbyclassname method to assign a class, and tidy up ready for codebar

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let currentDate = new Date();
 let hours = currentDate.getHours(); //extract the hour from currentDate
 
 let happyResponse = document.querySelectorAll('.happy_response');
-let sadResponse = document.querySelectorAll('.sad_response');
+let sadResponse = document.getElementsByClassName('sad_response');
 //this generates a random number for use as an index
 let randomIndex = Math.floor((Math.random() * 2));
 
@@ -25,20 +25,23 @@ let moodQuestions = {
 
 console.log(hours); //logs hour
 
+// On click remove options, display question from list and input field
+for (i=0; i < sadResponse.length; i++){
+  sadResponse.onclick = function() {
+    console.log("Sad Pressed");}
+  };
+
 // function questionField(feeling) {
 //   console.log(moodQuestions.feeling[randomIndex])
+// }
+
+// function chooseHappy(e) {
+//   if(e.propertyName !== 'click') return; //to skip over events clicks
+//   console.log("Choose Happy");
 // };
-
-function chooseHappy(e) {
-  if(e.propertyName !== 'click') return; //to skip over events clicks
-  console.log("Choose Happy");
-};
-
-
-happyResponse.forEach(click =>  click.addEventListener('mousedown', chooseHappy));
-
-
-
+//
+//
+// happyResponse.forEach(click =>  click.addEventListener('mousedown', chooseHappy));
 
 // for (i=0; i < sadResponse.length; i++){
 //   sadResponse.onclick = function() {
