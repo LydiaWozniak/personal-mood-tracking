7eb593610378eb640ddc63200e98ef60ccf77219
Lydia Wozniak <lydiawozniak@lydias-mbp.home>
2018-01-20 22:27:14 +0000

    Made response buttons call random question, question is displayed, and input field is shown whilst response selection is hidden

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let currentDate = new Date();
 let hours = currentDate.getHours(); //extract the hour from currentDate
 console.log(hours);
 
-// let happyResponse = document.querySelectorAll('.happy_response');
-// let sadResponse = document.getElementsByClassName('sad_response');
+let questionField = document.querySelector('#question');
+let sadResponse = document.querySelector('.sad_response');
+let happyResponse = document.querySelector('.happy_response');
+let moodSelection = document.querySelector('.response');
+let inputField = document.querySelector('.inputField');
 //this generates a random number for use as an index
 let randomIndex = Math.floor((Math.random() * 2));
 
@@ -26,47 +29,23 @@ let moodQuestions = {
 
 console.log(hours); //logs hour
 
-function happyFunction(){
-  console.log('yay');
+function sadQuestion(){
+  let response = `${moodQuestions.sad[randomIndex]}`;
+  question.textContent = response;
+  moodSelection.classList.toggle("hidden");
+  inputField.classList.toggle("hidden");
+};
+
+function happyQuestion(){
+  let response = `${moodQuestions.happy[randomIndex]}`;
+  question.textContent = response;
+  moodSelection.classList.toggle("hidden");
+  inputField.classList.toggle("hidden");
 };
 
 // function sadFunction(){
 //   console.log('Noooooo');
 // }
 
-document.getElementById("goodButton").addEventListener("click", happyFunction);
-
-// On click remove options, display question from list and input field
-// for (i=0; i < sadResponse.length; i++){
-//   sadResponse.onclick = function() {
-//     console.log("Sad Pressed");}
-//   };
-
-// document.ready(function(){
-//   sadResponse.onclick = function(){
-//     console.log('Sad pressed');
-//   };
-// });
-
-// function questionField(feeling) {
-//   console.log(moodQuestions.feeling[randomIndex])
-// }
-
-// function chooseHappy(e) {
-//   if(e.propertyName !== 'click') return; //to skip over events clicks
-//   console.log("Choose Happy");
-// };
-//
-//
-// happyResponse.forEach(click =>  click.addEventListener('mousedown', chooseHappy));
-
-// for (i=0; i < sadResponse.length; i++){
-//   sadResponse.onclick = function() {
-//     let msg = moodQuestions.sad[randomIndex];
-//     function updateQuestion() {
-//       let questionField = document.getElementById('question');
-//       questionField.textContent = msg;
-//     }
-//   };
-// };
-// console.log(moodQuestions.happy[randomIndex]);
+sadResponse.addEventListener("click", sadQuestion);
+happyResponse.addEventListener("click", happyQuestion);
